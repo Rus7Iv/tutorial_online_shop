2638b060a0ba02840ee6d49660b3c28f9fa410a8
Rus7Iv <rusivary@gmail.com>
2022-11-26 13:16:03 +0400

    Lesson 2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello!</h1>
+    <v-main-wrapper />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import vMainWrapper from './components/v-main-wrapper.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    vMainWrapper
   }
 }
 </script>
